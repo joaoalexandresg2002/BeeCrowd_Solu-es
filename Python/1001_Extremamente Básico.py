66c3caf0674e7d977b2323a5896285d7da0a05fe
joaoalexandresg2002 <joaoalexandresg2025@gmail.com>
2025-09-26 22:10:31 -0300

    Solucionado

---
+++ Python/1001_Extremamente Básico.py
@@ -0,0 +1,8 @@
+# Lê um número inteiro da entrada e armazena na variável 'a'.
+a = int(input())
+# Lê outro número inteiro da entrada e armazena na variável 'b'.
+b = int(input())
+
+# Calcula a soma de 'a' e 'b' e imprime no formato solicitado.
+# O resultado será exibido como: X = <valor da soma>.
+print("X =", a + b)
