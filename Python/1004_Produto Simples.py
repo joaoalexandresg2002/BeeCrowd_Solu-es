5a96b82e463c03960251ec4f69a79c9477c2ba72
joaoalexandresg2002 <joaoalexandresg2025@gmail.com>
2025-09-26 22:47:50 -0300

    feat: adicionar solução para o problema 1004 (Python)

---
+++ Python/1004_Produto Simples.py
@@ -0,0 +1,10 @@
+# Lê o primeiro número inteiro digitado pelo usuário
+a = int(input())
+# Lê o segundo número inteiro
+b = int(input())
+
+# Calcula o produto (multiplicação) entre os dois números
+prod = a * b
+
+# Mostra o resultado na tela
+print("PROD =", prod)
